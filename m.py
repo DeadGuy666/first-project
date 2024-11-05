7d89a550aab6cf07d8838c2d904ca57dbd845fee
DeadGuy666 <lucasvergarabarbero@gmail.com>
2024-11-05 19:15:41 +0100

    Update m.py

--- m.py
+++ m.py
@@ -1,7 +1,11 @@
 meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
-            }
+            "ROFL" : "una respuest-a a una broma",
+            "SHEESH" : "ligera desaprobación",
+            "CREEPY" : "aterrador, siniestro",
+            "AGGRO" : "ponerse agresivo/enojado",
+}
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 if word in meme_dict.keys():
     print("Lo que significa tu palabra es:", meme_dict[word])
